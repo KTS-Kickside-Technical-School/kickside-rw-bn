8da04e0c687abd0759aae696b22f637dd3afa5e3
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-01-04 16:14:20 +0200

    Fix: Remove return

--- src/controllers/articlesControllers.ts
+++ src/controllers/articlesControllers.ts
@@ -208,21 +208,20 @@ const postArticleComment = async (req: any, res: Response): Promise<any> => {
     }
 };
 
-const deleteArticle = async(req: any, res: Response , next:NextFunction): Promise<any> =>{
+const deleteArticle = async (req: any, res: Response, next: NextFunction): Promise<any> => {
     try {
         const deletedArticle = await articlesRepositories.deleteArticle(req.article._id)
         return res.status(200).json({
             status: 200,
             message: "Article deleted successfully",
-            deletedData: {deletedArticle}
         })
-        
+
     } catch (error) {
         res.status(500).json({
             status: 500,
             message: error.message
         })
-        
+
     }
 }
 
