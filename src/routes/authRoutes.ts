350668f98baca054f217bbf11f759f02a4588652
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-01-06 07:20:23 +0200

    Fix: Reset password & Logout (#25)

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { isUserExists } from '../middlewares/authMiddleware';
 import bodyValidation from '../middlewares/bodyValidation';
-import { forgotPasswordSchema, resetPasswordSchema, userLoginSchema } from '../validations/authValidations';
+import { forgotPasswordSchema, logoutSchema, resetPasswordSchema, userLoginSchema } from '../validations/authValidations';
 import authControllers from '../controllers/authControllers';
+import { userAuthorization } from '../middlewares/authorization';
 
 const authRoute = express.Router();
 
 authRoute.post("/login", bodyValidation(userLoginSchema), isUserExists, authControllers.userLogin);
 
-authRoute.post("/forgot-password", bodyValidation(forgotPasswordSchema),isUserExists, authControllers.forgotPassword)
+authRoute.post("/forgot-password", bodyValidation(forgotPasswordSchema), isUserExists, authControllers.forgotPassword)
 authRoute.post("/reset-password", bodyValidation(resetPasswordSchema), authControllers.resetPassword)
 
+authRoute.post("/logout", userAuthorization(["Admin", "Journalist", "Editor"]), authControllers.userLogout)
 export default authRoute
