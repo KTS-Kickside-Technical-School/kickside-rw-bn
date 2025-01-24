acb4ceba8b5d55e64ae0243028092ad8386775cc
Diiboi <dieudonneniyonkuru2018@gmail.com>
2025-01-24 12:59:40 +0200

    fixed inquiry status

--- src/validations/inquiryVlidation.ts
+++ src/validations/inquiryVlidation.ts
@@ -1,14 +1,13 @@
 import Joi from "joi";
 
 export const inquirySchema = Joi.object({
-    firstName: Joi.string().required(),
-    lastName: Joi.string(),
-    email: Joi.string().required(),
-    topic: Joi.string().required(),
-    message: Joi.string().required(),
-
-})
+  firstName: Joi.string().required(),
+  lastName: Joi.string(),
+  email: Joi.string().required(),
+  topic: Joi.string().required(),
+  message: Joi.string().required(),
+});
 
 export const updateInquirySchema = Joi.object({
-    status: Joi.string().required().valid("pending", "accepted", "solved")
-})
+  status: Joi.string().required().valid("pending", "solved"),
+});
