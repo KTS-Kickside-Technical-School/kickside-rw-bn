c6b1e88cbc2a9014bdfcebe95ee2442b5dd696bc
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-07-27 11:57:48 +0800

    AA (#68)

--- src/database/seeds/index.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from '../config/config';
-import seedArticles from './articles';
-import seedUsers from './users';
-
-connect().then(async () => {
-    await seedUsers();
-    await seedArticles();
-    process.exit()
-})
--- src/middlewares/articlesMiddleware.ts
+++ src/middlewares/articlesMiddleware.ts
@@ -1,27 +1,6 @@
-import { Request, Response, NextFunction } from "express";
+import {  Response, NextFunction } from "express";
 import articlesRepositories from "../repository/articlesRepositories"
 import mongoose from "mongoose";
-import Article from "../database/models/article";
-
-
-
-export const isArticleAlreadyExists = async (req: any, res: Response, next: NextFunction): Promise<any> => {
-    try {
-        const article = await articlesRepositories.findArticleByAttribute("title", req.body.title);
-        if (article) {
-            return res.status(400).json({
-                status: 400,
-                message: "Article already exists"
-            })
-        }
-        return next();
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message
-        })
-    }
-}
 
 export const isArticleExists = async (req: any, res: Response, next: NextFunction): Promise<any> => {
     try {
@@ -86,98 +65,7 @@ export const isArticleExistsBySlug = async (req: any, res: Response, next: NextF
     }
 };
 
-export const isArticleOwned = async (req: any, res: any, next: NextFunction): Promise<any> => {
-    try {
-        const isArticleOwned = req.article.author._id.equals(req.user._id);
-
-        if (!isArticleOwned) {
-            return res.status(403).json({
-                status: 403,
-                message: 'Article not owned by user',
-            });
-        }
-
-        next();
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message,
-        });
-    }
-};
-
-export const isArticleEditRequestAlreadyExists = async (req: any, res: Response, next: NextFunction): Promise<any> => {
-    try {
-        const editRequest = await articlesRepositories.findArticleEditRequestByAttribute("article", req.article._id);
-
-        if (editRequest && editRequest.isAccepted === false) {
-            return res.status(400).json({
-                status: 400,
-                message: "Edit request already exists!"
-            })
-        }
-
-        return next();
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message
-        })
-    }
-}
-
-export const isArticleEditable = async (req: any, res: Response, next: NextFunction): Promise<any> => {
-    try {
-        if (req.article.isEditable === false) {
-            return res.status(400).json({
-                status: 400,
-                message: 'Article is not editable'
-            })
-        }
-        return next();
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            mmessage: error.message
-        })
-    }
-}
-
-export const isArticleEditRequestExistsAndPending = async (req: any, res: Response, next: NextFunction): Promise<any> => {
-    try {
-        const { id } = req.params;
-
-        if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({
-                status: 400,
-                message: "Invalid parameters"
-            });
-        }
-
-        const editRequest = await articlesRepositories.findArticleEditRequestByAttribute("_id", id);
-        if (!editRequest) {
-            return res.status(404).json({
-                status: 404,
-                message: "Article edit request not found"
-            })
-        }
-        req.editRequest = editRequest
-        return next();
-
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message
-        })
-    }
-}
-
-export const isArticleHaveComments = async (article: any) => {
-    const comments = await articlesRepositories.findArticleComments(article);
-    return comments
-}
-
-export const isAreticlesExistsByCategory = async (req: any, res: Response, next: NextFunction): Promise<any> => {
+export const isArticlesExistsByCategory = async (req: any, res: Response, next: NextFunction): Promise<any> => {
     const { category } = req.params
     if (!category) {
         return res.status(400).json({
@@ -196,3 +84,7 @@ export const isAreticlesExistsByCategory = async (req: any, res: Response, next:
     next();
 }
 
+export const isArticleHaveComments = async (article: any) => {
+    const comments = await articlesRepositories.findArticleComments(article);
+    return comments
+}
